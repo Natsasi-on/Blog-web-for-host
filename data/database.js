d2104e7ff3add31b361ae8fdfa878a7b0da5630b
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-03-01 19:39:31 -0500

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    database: 'blog',
-    user: 'root',
-    password: ''
+    host: 'buiwarp0roi2pe7kywvn-mysql.services.clever-cloud.com',
+    database: 'buiwarp0roi2pe7kywvn',
+    user: 'uaj0ljkkrp0ymah5',
+    password: 'mSNRRbvOz84R4UbnwF4D'
 });
 
 module.exports = pool;
