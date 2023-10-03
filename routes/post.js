612eafe1cc48398298b410f68b89fba4f30c9530
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-03 03:00:14 -0400

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
 const db = require('../data/database');
 const router = express.Router();
 
+// Render the index page
 router.get('/', function (req, res) {
     res.render('index');
 });
 
+// Retrieve and render posts from the database
 router.get('/index', async function (req, res) {
     const query = `SELECT posts.*, authors.name AS author_name FROM posts 
     INNER JOIN authors ON posts.author_id = authors.id`;
@@ -14,6 +15,7 @@ router.get('/index', async function (req, res) {
     res.render('index', { posts: posts });
 });
 
+// Handle the creation of a new post
 router.post('/index', async function (req, res) {
     const data = [
         req.body.title,
@@ -25,14 +27,13 @@ router.post('/index', async function (req, res) {
     res.redirect('/index');
 });
 
-//use await so async is work like sync so it wait until complete
+// Render the add-post page with a list of authors
 router.get('/add-post', async function (req, res) {
-    // the data from db will show in [] show we add [] to variable
-    // const result = await db.query('SELECT * FROM authors');
     const [myauthors] = await db.query('SELECT * FROM authors');
     res.render('add-post', { authors: myauthors });
 });
 
+// Render the edit-post page for a specific post
 router.get('/posts/:id/edit', async function (req, res) {
     const query = `SELECT * FROM posts WHERE id = ?`;
     const [posts] = await db.query(query, [req.params.id]);
@@ -44,14 +45,15 @@ router.get('/posts/:id/edit', async function (req, res) {
     res.render('edit-post', { post: posts[0] });
 });
 
+// Handle the update of a post
 router.post('/posts/:id/edit', async function (req, res) {
     const query = `UPDATE posts SET title =?, summary=?, body = ? WHERE id =?`;
-    db.query(query, [req.body.title, req.body.summary, req.body.postContent, req.params.id,]);
+    db.query(query, [req.body.title, req.body.summary, req.body.postContent, req.params.id]);
 
     res.redirect('/index');
 });
 
-
+// Render the post-detail page for a specific post
 router.get('/posts/:id', async function (req, res) {
     const query = `SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
     INNER JOIN authors ON posts.author_id = authors.id
@@ -77,6 +79,7 @@ router.get('/posts/:id', async function (req, res) {
     res.render('post-detail', { post: postData });
 });
 
+// Handle the deletion of a post
 router.post('/posts/:id/delete', async function (req, res) {
     await db.query('DELETE FROM posts WHERE id = ?', [req.params.id]);
 
